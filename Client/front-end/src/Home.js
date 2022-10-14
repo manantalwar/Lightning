4c06026c11afd715473da55c3a6020d001cc7ade
GabeSussman <gsussman@umass.edu>
2022-10-14 19:14:16 -0400

    updated

--- Client/front-end/src/Home.js
+++ Client/front-end/src/Home.js
@@ -1,9 +1,23 @@
 import Navbar from './Navbar';
+import './home.css';
 
 const home = () => {
     const page = "Home"
     return (  
-        <Navbar page={page}/>
+        <div classname="Home">
+            <div>
+                <Navbar page={page}/>
+            </div>
+            <div classname="container">
+                <div classname="Box">
+                    <button>
+                    
+                    </button>
+                    <button></button>
+                </div>
+            </div>
+
+        </div>
     );
 }
  
