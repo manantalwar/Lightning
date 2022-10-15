e2314bc555b3468c1fc6eb7d9364299c8d9c438e
GabeSussman <gsussman@umass.edu>
2022-10-15 17:44:22 -0400

    Filter page framework

--- client/src/Filter.js
+++ client/src/Filter.js
@@ -5,14 +5,44 @@ import './Filter.css'
 const Filter = () => {
     const page = "Filter"
     const [filters, setFilters] = useState([]);
+    const scenarios = ['select scenario', 'a', 'b', 'c'];
     return (  
         <div className="filter">
             <Navbar page={page}/>
-            <div className="containter">
-                <div className="filterList">
-                    
+            <div className="content">
+                <div className="list">
+                    <p className="topTitle">Active Filters</p>
+                    <div className="filterList">
+                            
+                    </div>
+                    <button className="submit">Submit</button>
                 </div>
-                <button className="submit">Submit</button>
+                <div className='filters'>
+                    <p className='topTitle'>Custom Filters</p>
+                    <input className='filterInputs' type='text' value='key'></input>
+                    <input className='filterInputs' type='text' value='value'></input>
+                    <button className='add'>Add Filter</button>
+                    <p className='scenarioTitle'>Scenarios</p>
+                    <select className='filterInputs'>
+                        {scenarios.map((scenario) => (
+                            <option value={scenario}>{scenario}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className='filters'>
+                    <p className='topTitle'>Dates</p>
+                    <input className='filterInputs' type='date'></input>
+                    <input className='filterInputs' type='date'></input>
+                    <input className='filterInputs' type='time'></input>
+                    <input className='filterInputs' type='time'></input>
+                    <button className='add'>Add Filter</button>
+                </div>
+                <div className='filters'>
+                    <p className='topTitle'>LMP</p>
+                    <input className='filterInputs' type='text' value='start'></input>
+                    <input className='filterInputs' type='text' value='end'></input>
+                    <button className='add'>Add Filter</button>
+                </div>
             </div> 
         </div>
     );
