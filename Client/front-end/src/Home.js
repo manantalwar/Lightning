2e8b7e30fb7314901d3ef73c50c083561902e05d
Alan <azzheng@umass.edu>
2022-10-15 00:36:29 -0400

    Implemented the home page layout

--- Client/front-end/src/Home.js
+++ Client/front-end/src/Home.js
@@ -1,19 +1,30 @@
+import React, { useEffect, useState } from "react";
 import Navbar from './Navbar';
 import './home.css';
 
-const home = () => {
+const Home = ({props}) => {
+    const [hover1, setHover1] = useState(false);
+    const [hover2, setHover2] = useState(false);
     const page = "Home"
     return (  
-        <div classname="Home">
-            <div>
-                <Navbar page={page}/>
-            </div>
-            <div classname="container">
-                <div classname="Box">
-                    <button>
-                    
+        <div className="home">
+            <Navbar page={page}/>
+            <div className="container">
+                <div className="box">
+                    <button 
+                        className="mainbtn" 
+                        onMouseEnter={() => setHover1(true)}
+                        onMouseLeave={() => setHover1(false)}>
+                        {!hover1 && <div>Validation</div>}
+                        {hover1 && <div>Validating simulation data as compared to base case data. Displays graphs comparing the data sets and shows useful statitsics comparing the data.</div>}
                     </button>
-                    <button></button>
+                    <button 
+                        className="mainbtn"
+                        onMouseEnter={() => setHover2(true)}
+                        onMouseLeave={() => setHover2(false)}>
+                        {!hover2 && <div>Simulation</div>}
+                        {hover2 && <div>Filters datasets based on chosen queries and displays graphs and useful information about remaining nodes.</div>}
+                    </button>
                 </div>
             </div>
 
@@ -21,4 +32,4 @@ const home = () => {
     );
 }
  
-export default home;
+export default Home;
