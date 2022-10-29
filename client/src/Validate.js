b43533b5e715096a53efc02f9a7e8b41b077a5e6
Alan <azzheng@umass.edu>
2022-10-29 14:23:14 -0400

    Fixed the sizing of the graphs

--- client/src/Validate.js
+++ client/src/Validate.js
@@ -1,7 +1,8 @@
 import Navbar from './Navbar';
 import './Validate.css'
 
-import { HeatMap, ScatterPlot, Histogram, LineChart } from './Graphs';
+import { HeatMap, ScatterPlot, Histogram} from './Graphs';
+import LineChart from './Graphs';
 
 const Validation = () => {
     const page = "Validation"
@@ -25,36 +26,26 @@ const Validation = () => {
                     <button className='addB'>Update Filter</button>            
                 </div>
                 
-                <div className='twoGraphs'>
+                <div className='graphs'>
                     <div className = 'graph'>
                         <LineChart />
                     </div>
-                    
-                    {/* <br></br> */}
                     <div className = 'graph'>
                         <ScatterPlot />
                     </div>
-                </div>
-                {/* <br></br> */}
-                <div className='twoGraphs'>                
                     <div className = 'graph'>
-                        <div className = 'histogram'>
                             <Histogram mainText={'Historgram: Base Case'} subText={'Base Case HUB Node Prices'}/>         
-                        </div>
-                        {/* <br></br> */}
-                        <div className = 'histogram'>
                             <Histogram mainText={'Historgram: Scenario'} subText={'HUB Node Prices of Scenario that we are Validating'}/>                        
-                        </div>
                     </div>
-                    <br></br>
                     <div className = 'graph'>
                         <HeatMap />
                     </div>
+                    
                 </div>
 
-                <div className ="data">
+                {/* <div className ="data">
                     <p className="metrics">Useful Information:<br /> Maximum Difference: <br /> Minimum Difference: <br /> Average Difference:  </p>
-                </div>  
+                </div>   */}
 
             </div>
         </div>
