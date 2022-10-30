15b5a889c1bf42d20bed82d1f865714b4c1411b5
cgenta20 <113484377+cgenta20@users.noreply.github.com>
2022-10-30 17:58:54 -0400

    Restful Api + Prisma
    1. Set up Figma with mongo
    2. Made the node-parser recursive for nested objects.
    3. /init and /get routes (returning the init object and every node respectively)

---
+++ server/prisma/RESTful.js
@@ -0,0 +1,53 @@
+const express= require("express");
+const prismadb = require("./prismadb.js");
+const app=express();
+app.listen(3000) // localhost for now
+
+const collection = "dummy_node_data"
+
+//Init Route: This Route supplies data to the Client For Filtration Initilization
+app.get('/init', (req, res, next)=>{
+    prismadb.prismaGetOne(collection).then(node => {
+
+        //Function returning the type of a field
+        function getType(field){
+            if(typeof(field) == "string" && !isNaN(field)){ //Identifies Numbers
+                return typeof(1);
+            } else if (field == true || field == false || field == "true" || field == "false"){ //Identifies Booleans
+                return typeof(true);
+            } else if (!isNaN(Date.parse(field))){ //Identifies Dates
+                return "date";
+            } else { //Otherwise we interpret as a string
+                return typeof(field);
+            }
+        }
+    
+        //Pulls all keys, and their types, from the first node, recursively for nested objects
+        function classify(obj){
+            let keys = {}
+            Object.keys(obj).forEach(function(key) {
+                if(!keys.hasOwnProperty(key)){
+                    keys[key] = getType(obj[key])
+                    if(keys[key] == "object"){
+                        sub = classify(obj[key])
+                        Object.keys(sub).forEach(function(skey) {
+                            let nkey = key + "/" + skey;
+                            keys[nkey] = sub[skey];
+                        })
+                    }
+                }
+            });
+            return keys
+        }  
+        res.send(classify(node))
+    })
+});
+
+
+app.get('/get', (req, res, next)=>{
+    filters = {}
+
+    prismadb.prismaGet(collection,filters).then(node => {
+        res.send(node)
+    })
+});
---
+++ server/prisma/prismadb.js
@@ -0,0 +1,31 @@
+const { PrismaClient } = require('@prisma/client')
+
+async function prismaGetOne(collection){
+    const prisma = new PrismaClient()
+    try{
+        const one = await prisma[collection].findFirst()
+
+        return one
+    }
+    finally{
+        await prisma.$disconnect()
+    }
+}
+
+//Filters Type Def:
+async function prismaGet(collection, filters = {}){
+    const prisma = new PrismaClient()
+    try{
+        const many = await prisma[collection].findMany(filters)
+
+        return many
+    }
+    finally{
+        await prisma.$disconnect()
+    }
+}
+
+module.exports={
+    prismaGetOne,
+    prismaGet
+}
