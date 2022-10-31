0e0fff4055da131921d81acba22ddf978c303daf
atifabedeen <atifza123@gmail.com>
2022-10-31 01:33:17 -0400

    Pop up window for graphs

--- client/src/Validate.js
+++ client/src/Validate.js
@@ -1,5 +1,7 @@
+import React, {useState} from 'react'
 import Navbar from './Navbar';
 import './Validate.css'
+import Modal from './Modal'
 
 import { HeatMap, ScatterPlot, Histogram} from './Graphs';
 import LineChart from './Graphs';
@@ -7,6 +9,11 @@ import LineChart from './Graphs';
 const Validation = () => {
     const page = "Validation"
     const scenarios = ['a', 'b', 'c'];
+    const [isOpen1, setIsOpen1] = useState(false)
+    const [isOpen2, setIsOpen2] = useState(false)
+    const [isOpen3, setIsOpen3] = useState(false)
+    const [isOpen4, setIsOpen4] = useState(false)
+
     return (  
         <div className="validation">
             <Navbar page={page}/>
@@ -29,16 +36,44 @@ const Validation = () => {
                 <div className='graphs'>
                     <div className = 'graph'>
                         <LineChart />
+                        <button onClick={() => setIsOpen1(true)}>Expand Graph</button>
+                        <Modal open={isOpen1} onClose={() => setIsOpen1(false)}>
+                            <div>
+                                <LineChart />
+                            </div>
+                        </Modal>
                     </div>
                     <div className = 'graph'>
                         <ScatterPlot />
+                        <button onClick={() => setIsOpen2(true)}>Expand Scatter</button>
+                        <Modal open={isOpen2} onClose={() => setIsOpen2(false)}>
+                            <div>
+                                <ScatterPlot />
+                            </div>
+
+                     </Modal>  
                     </div>
                     <div className = 'graph'>
                             <Histogram mainText={'Historgram: Base Case'} subText={'Base Case HUB Node Prices'}/>         
-                            <Histogram mainText={'Historgram: Scenario'} subText={'HUB Node Prices of Scenario that we are Validating'}/>                        
+                            <Histogram mainText={'Historgram: Scenario'} subText={'HUB Node Prices of Scenario that we are Validating'}/>  
+                            <button onClick={() => setIsOpen3(true)}>Expand Graph</button> 
+                            <Modal open={isOpen3} onClose={() => setIsOpen3(false)}>
+                            <div>
+                                <Histogram mainText={'Historgram: Base Case'} subText={'Base Case HUB Node Prices'}/>         
+                                <Histogram mainText={'Historgram: Scenario'} subText={'HUB Node Prices of Scenario that we are Validating'}/> 
+                            </div>
+
+                     </Modal>                                                  
                     </div>
                     <div className = 'graph'>
                         <HeatMap />
+                        <button onClick={() => setIsOpen4(true)}>Expand Graph</button>
+                        <Modal open={isOpen4} onClose={() => setIsOpen4(false)}>
+                            <div>
+                                <HeatMap />
+                            </div>
+
+                     </Modal>                        
                     </div>
                     
                 </div>
