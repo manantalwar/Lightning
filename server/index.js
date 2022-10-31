419c618da1f45a982ae6a9687a7b51cddb6b5e2f
Alan <azzheng@umass.edu>
2022-10-31 00:14:33 -0400

    Added a routing file package.json to server

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import logger from 'morgan';
+import axios from 'axios';
+
+const port = 4000;
+const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+
+
+/*
+  template for an endpoint
+
+  app.get('/name', async(req, res) => {
+    try {
+      const object1 = req.body;
+      res.status(200).send(object1);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+*/
+
+app.get('/test', async(req, res) => {
+  try {
+    const object1 = req.body;
+    res.status(200).send("success");
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
