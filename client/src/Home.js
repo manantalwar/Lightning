ac724df9e887b2a7eed9a0bec98d01d3db673634
Nishant <nishant1549@gmail.com>
2022-11-04 19:08:20 -0400

    sample path

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Navbar from './Navbar';
 import './Home.css';
+import axios from 'axios';
+
+// const path = require('path');
 
 const Home = ({props}) => {
     const [hover1, setHover1] = useState(false);
     const [hover2, setHover2] = useState(false);
     const page = "Home"
+
+    // this function is fired when validation button is clicked.
+    const clickValidation = async (event) => {
+    event.preventDefault();
+      await axios.post(`http://localhost:3000/filter`, {
+        // sendFile: path.join(__dirname, "public", "Home.js")
+        sendFile: ".\lightning\client\src\Validate.js"
+      });
+    };
+
     return (  
         <div className="home">
             <Navbar page={page}/>
@@ -14,7 +27,8 @@ const Home = ({props}) => {
                     <button 
                         className="mainbtn" 
                         onMouseEnter={() => setHover1(true)}
-                        onMouseLeave={() => setHover1(false)}>
+                        onMouseLeave={() => setHover1(false)}
+                        onClick={() => clickValidation}>
                         {!hover1 && <div>Validation</div>}
                         {hover1 && <div>Validating simulation data as compared to base case data. Displays graphs comparing the data sets and shows useful statitsics comparing the data.</div>}
                     </button>
