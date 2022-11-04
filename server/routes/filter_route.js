309965f2734c4c00cdc74c551522d1d413aec780
cgenta20 <113484377+cgenta20@users.noreply.github.com>
2022-11-03 22:14:41 -0400

    Built The GET OBJECT => PRISMA Object for number fields

--- server/routes/filter_route.js
+++ server/routes/filter_route.js
@@ -1,40 +1,57 @@
-const router= require("express").Router()
+const router = require("express").Router()
 
 //get: Returns Nodes according to a query
 //TODO: Parse Query Fields for Prismas filtration object
 //URL Query Form: /get?...
 
 // Filters is a JSON object. Whichever category needs a range component, needs to have an empty object inside the where part. For example, LMP
-let filters={
-    where:
-        {
-            LMP:{
-            }
-        }
-    }
+
 router.route('/').get((req, res, next)=>{
-    const test = req.query
+    const q = req.query
+    //console.log(q)
     let RESTful=req.RESTful
     let collection=req.collection
     let init=req.init
-   // console.log(test)
-    console.log(init)
-    assign("SCENARIO_ID", '2') //assign with query type and 
-    gtltFilter("LMP",33, 32)
-    RESTful.Get(collection,filters).then(nodes => {
+    //console.log(test)
+    let filters = {
+        where:{
+
+        }
+    }
+
+    //assign(filters,"SCENARIO_ID", '2')
+
+    for(key in q){
+        if(init[key].toString() == "number"){
+            if(typeof(q[key]) == "object"){
+                console.log(q[key])
+                gtltFilter(filters, key.toString(), q[key][0], q[key][1])
+            }
+            else{
+                if(!isNaN(q[key])){
+                    assign(filters, key.toString(), q[key])
+                }
+            }
+        }
+    }
+    console.log(filters)
+    RESTful.Get(collection, filters).then(nodes => {
         res.send(nodes)
     }).catch(err=>res.status(400).json('Error: ' +err))
 });
 
-function assign(queryType, filterVal){
-    filters.where[queryType]=filterVal;
+function assign(obj, queryType, filterVal){
+    obj.where[queryType]=filterVal;
 }
+
 // Push empty object if 
-function gtltFilter(queryType, ltVal, gtVal){
-     const gt="gt"
-     const lt="lt"
-     filters.where[queryType][lt]=ltVal.toString()
-     filters.where[queryType][gt]=gtVal.toString()
+function gtltFilter(obj, queryType, gtVal, ltVal){
+    obj.where[queryType] = {
+        gt:gtVal.toString(),
+        lt:ltVal.toString()
+    }
+    //[lt.toString()] = 
+    //obj.where[queryType][gt.toString()] = 
 }
 
 module.exports=router
