807c8c37782c55a1511dccf9be4f63690d88272c
cgenta20 <113484377+cgenta20@users.noreply.github.com>
2022-11-05 15:46:01 -0400

    Change Format of PRISMA object to allow for compound queries

--- server/routes/filter_route.js
+++ server/routes/filter_route.js
@@ -15,25 +15,45 @@ router.route('/').get((req, res, next)=>{
     //console.log(test)
     let filters = {
         where:{
-
+            AND:[] //{OR:[{LMP:'30.1'},{LMP:'30.2'}]},{SCENARIO_ID:1}]
         }
     }
 
     //assign(filters,"SCENARIO_ID", '2')
-
+    // THIS HANDLES NUMBERS /filter?field=val returns every entry where field == val
+    // /filter?field=val&field=val2 returns every entry where val <= field <= val2
+    // /filter?field=SELECT&field=val&field=val2... returns every entry where field == val OR field == val2
+    // /NOW WORKS WITH COMPOUND SELECTION
     for(key in q){
         if(!init.hasOwnProperty(key)){continue;}
-        if(init[key].toString() == "number"){
+        if(init[key].toString() == "number"){ 
             if(typeof(q[key]) == "object"){
-                gtltFilter(filters.where, key.toString(), q[key][0], q[key][1])
+                if(q[key][0].toLowerCase() == 'select'){
+                    let obj = {OR:[]}
+                    q[key].forEach((elem) => {
+                        if(!isNaN(elem)){
+                            let obj2 = {}
+                            obj2[key.toString()] = elem
+                            obj.OR.push(obj2)
+                        }
+                    });
+                    filters.where.AND.push(obj)
+                }else{
+                    let obj = {}
+                    gtltFilter(obj, key.toString(), q[key][0], q[key][1])
+                    filters.where.AND.push(obj)
+                }
             }
             else{
                 if(!isNaN(q[key])){
-                    assign(filters.where, key.toString(), q[key])
+                    let obj = {}
+                    assign(obj, key.toString(), q[key])
+                    filters.where.AND.push(obj)
                 }
             }
         }
     }
+    
     console.log(q)
     console.log(filters)
 
