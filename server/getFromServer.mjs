502aa6e2f63094888b82481e00d6b52fc1b0bb35
cgenta20 <113484377+cgenta20@users.noreply.github.com>
2022-11-06 18:24:06 -0500

    Update getFromServer.mjs

--- server/getFromServer.mjs
+++ server/getFromServer.mjs
@@ -8,7 +8,7 @@ const initPath = '/init'
 
 /*
     Due to the asynchonous nature of http calls.
-    We must handle our data within promises.
+    We must handle our data within promises and then() calls.
     GOOD: pullNodes().then((nodes) => console.log(nodes))
     BAD: console.log(pullNodes())
 */
