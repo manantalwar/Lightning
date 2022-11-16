001cac99ebeb01aa65a62dae92ab554a72df74f7
AaditBhatia <aadit.bhatia93@gmail.com>
2022-11-16 10:54:54 -0500

    Update time.js

--- server/time.js
+++ server/time.js
@@ -4,25 +4,19 @@
 // filter on these dates
 
 function takeIntoAccountDaylightSavings(localTimeString){
-
-    // convert to msec since Jan 1 1970
-    //const d = new Date()
+    
+    const getstringToDate = (localTimeString) => {
+        // input format is- 17-JUL-2020 01
+        // convert string to date 
+        // Date Syntax: new Date(year value, IndexOfMonth, day value, hours, minutes, seconds)
+        const [dateValues, timeValues] = localTimeString.split(' ');
+        const [day, month, year] = dateValues.split('-');
+        var months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+        monthNum = months.indexOf(month.toLowerCase());
+        const date = new Date(year,monthNum,day, timeValues);
   
-    const d=stringToTime(localTimeString);
-    const localTime = d.getTime()
-    // obtain local UTC offset and convert to msec
-    const localOffset = d.getTimezoneOffset() * 60 * 1000
-
-    // obtain UTC time in msec
-    const utcTime = localTime + localOffset
-
-    // obtain and add destination's UTC time offset
-    const estOffset = getEstOffset()
-    const usa = utcTime + (60 * 60 * 1000 * estOffset)
-
-    // convert msec value to date string
-    const nd = new Date(usa)
-
+        return date;
+    }
     // Get time zone offset for NY, USA
     const getEstOffset = () => {
         const stdTimezoneOffset = () => {
@@ -43,20 +37,35 @@ function takeIntoAccountDaylightSavings(localTimeString){
             return -5
         }
     }
-
-
-    const stringToDate = (localTimeString) => {
-        // input format is- 17-JUL-2020 01
-        // convert string to date 
-        // Date Syntax: new Date(year value, IndexOfMonth, day value, hours, minutes, seconds)
-        const [dateValues, timeValues] = localTimeString.split(' ');
-        const [day, month, year] = dateValues.split('-');
-        var months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
-        monthNum = months.indexOf(month.toLowerCase());
-        const date = new Date(year,monthNum,day, timeValues);
+    // convert to msec since Jan 1 1970
+    //const d = new Date()
   
-        return date;
-    }
+    const d=getstringToDate(localTimeString);
+    //const d=new Date(2022,11,06, 1);
+    const localTime = d.getTime()
+    // obtain local UTC offset and convert to msec
+    const localOffset = d.getTimezoneOffset() * 60 * 1000
+
+    // obtain UTC time in msec
+    const utcTime = localTime + localOffset
+
+    // obtain and add destination's UTC time offset
+    const estOffset = getEstOffset()
+    const usa = utcTime + (60 * 60 * 1000 * estOffset)
+
+    // convert msec value to date string
+    const nd = new Date(usa)
+
+    
+
+
+    
+    console.log(nd.getTime())
     return nd;
 }
+
+takeIntoAccountDaylightSavings('17-JUL-2020 01')
+takeIntoAccountDaylightSavings('06-Nov-2022 02')
+takeIntoAccountDaylightSavings('06-Nov-2022 01')
+takeIntoAccountDaylightSavings('06-Nov-2022 03')
 
