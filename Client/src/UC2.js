28351b186041625aba199a5884c1e983d5b74fbc
cgenta20 <113484377+cgenta20@users.noreply.github.com>
2022-11-23 20:09:08 -0500

    Stats box UC2

--- Client/src/UC2.js
+++ Client/src/UC2.js
@@ -3,25 +3,43 @@ import './UC2.css'
 import {aggregateNodes, pullNodes} from './getFromServer.mjs'
 import { useLocation } from "react-router-dom"
 import { useEffect, useState } from 'react'
+import {DataTable, PeroidButton} from './Graphs.js'
 
 const UC2 = (props) => {
 
     const page = 'Statistics'
-    const [nodes, setNodes] = useState([])
-    const [agr, setAgr] = useState([])
+    const [data, setData] = useState()
+    const [nodes, setNodes] = useState();
+    //const [agr, setAgr] = useState([])
     const [mean, setMean] = useState([])
     const {init} = props;
     const location = useLocation()
+    const [metric, setMetric] = useState("LMP")
+    const [period, setPeriod] = useState("All")
+    const [metrics, setMetrics] = useState(["LMP","MW"]);
+    const [stateInit, setStateInit] = useState(init);
 
-    
     useEffect(() => {
-        
+        setStateInit(init);
         pullNodes(location.state).then((obj) => setNodes(obj))
-        aggregateNodes(location.state).then((obj => setAgr(obj)))
-        getMean()
-    }, [init, location.state])
+        aggregateNodes(location.state).then((obj => setData(obj)))
+        //getMean()
+    }, [location.state, init])
 
-    function getMean() {
+    useEffect(() => {
+        let arr = [];
+        for (let key in stateInit) {
+            //console.log(key)
+            let keystr = (key).toString().toLocaleLowerCase();
+            if (stateInit[key] === 'number' && !keystr.includes("id")) {
+                arr.push(key)
+            }
+        }
+        setMetrics(arr);
+    }, [stateInit, data])
+    
+    
+    /* function getMean() {
         if(agr !== undefined){
             let arr = []
             Object.keys(agr).forEach((key) => {
@@ -37,16 +55,34 @@ const UC2 = (props) => {
             })
             setMean(arr)
         }
-    }
+    } */
     
 
     return (  
         <div className="UC2">
             <Navbar page={page}/>
             <div className="content">
+            <div className='dateTime'>
+                    <select className='metricSelector'
+                            onChange={(e) => setMetric(e.target.value)}>
+                        {metrics.map((metric) => (
+                            <option key={metric.toString()} value={metric}>{metric}</option>
+                        ))}
+                    </select> 
+                </div>
+
                 {/* <div>{nodes[0]</div> */}
-                <div className="values">{nodes.map((node) => (JSON.stringify(node)))}</div>
-            </div>
+                <div className = 'statBox'>
+                        <header className='StatsTitle'>Statistics ({metric})</header>
+                        <br/>
+                        <PeroidButton setParentPeriod={(per) => setPeriod(per)}/>
+                        <br/>
+                        <DataTable period={period} data={data} metric={metric}/>
+                    </div>
+                    <bre/>
+                </div>
+
+                <div className="values">{nodes?.map((node) => (JSON.stringify(node)))}</div>    
         </div>
     )
 }
